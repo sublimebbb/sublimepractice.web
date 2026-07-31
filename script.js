f6ce51654b9a7448c6af59892056a563a61a670b
sublime <workwithsublime@gmail.com>
2026-07-31 23:56:20 +0800

    Shorten chat popup title to ask

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const popups = {
     `
   },
   chat: {
-    title: "ask me anything",
+    title: "ask",
     html: `<p>send me a note at <a href="mailto:workwithsublime@gmail.com">workwithsublime@gmail.com</a></p>`
   }
 };
